b52143b6ed90addc4aa1cbe93c0ce584892dc7ce
unknown <eirik.broen@OSL-71867.onshore.pgs.com>
2014-08-12 11:19:08 +0200

    Built package

---
+++ dist/docs/js/docs-setup.js
@@ -0,0 +1,42 @@
+NG_DOCS={
+  "sections": {
+    "api": "API Documentation"
+  },
+  "pages": [
+    {
+      "section": "api",
+      "id": "adf.dashboard",
+      "shortName": "adf.dashboard",
+      "type": "object",
+      "moduleName": "adf",
+      "shortDescription": "The dashboard holds all structures and widgets.",
+      "keywords": "adf api dashboard holds object structures widgets"
+    },
+    {
+      "section": "api",
+      "id": "adf.dashboardProvider",
+      "shortName": "adf.dashboardProvider",
+      "type": "object",
+      "moduleName": "adf",
+      "shortDescription": "The dashboardProvider can be used to register structures and widgets.",
+      "keywords": "$injector adf alias angularjs api associated changes class closed column columns config configuration controller created css dashboard dashboardprovider default dependencies dependency description displayed edit factory false fn function html http injected instantiated invoke key loading loadingtemplate long map messages messagetemplate method mode module modus newly object optional org passed path predefined promise promises properties register registered registers rejected reload reloaded resolve resolved resources result return row rows scope service string structure structures styleclass template templateurl title treated true values wait widget widgets"
+    },
+    {
+      "section": "api",
+      "id": "adf.directive:adfDashboard",
+      "shortName": "adfDashboard",
+      "type": "directive",
+      "moduleName": "adf",
+      "shortDescription": "adfDashboard is a directive which renders the dashboard with all its ",
+      "keywords": "adf adfdashboard api attribute components dashboard directive div eca model renders requires store"
+    }
+  ],
+  "apis": {
+    "api": true
+  },
+  "html5Mode": false,
+  "startPage": "/api",
+  "scripts": [
+    "angular.min.js"
+  ]
+};
